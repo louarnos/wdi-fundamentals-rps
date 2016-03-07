d22c58c94c3174cbe0b35023d49ed495c1585a33
U-LouisArnos-PC\Louis Arnos <arnos.louis@gmail.com>
2016-03-06 21:10:30 -0500

    Wrote condition for getWinner()

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -44,7 +44,11 @@ function getWinner(playerMove,computerMove) {
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
+    if((playerMove == 'rock' && computerMove == "scissors") || (playerMove == 'paper' && computerMove == 'rock') || (playerMove == 'scissors' && computerMove =='paper')){
+    	winner = player;
+    } else if ((computerMove == 'rock' && playerMove == "scissors") || (computerMove == 'paper' && playerMove == 'rock') || (computerMove == 'scissors' && playerMove =='paper')){
+    	winner =  computer;
+    }else {winner = tie;}
     return winner;
 }
 
